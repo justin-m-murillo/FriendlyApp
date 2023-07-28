ccbf2616ec20b175513ab5d8a773ddb18b1bec43
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-07-28 08:34:56 -0700

    revert useFetchUser

--- src/hooks/useFetchUser/index.js
+++ src/hooks/useFetchUser/index.js
@@ -17,26 +17,18 @@ const useFetchUser = (id = '') => {
 
   useEffect(() => {
     const fetchUser = async () => {
-      let subscription;
       if (!id) {
         console.log('Fetching current user');
         const dbUser = await Auth.currentAuthenticatedUser();
-        id = dbUser.attributes.sub;
+        const thisUser = await DataStore.query(User, (u) => u.authUserId.eq(dbUser.attributes.sub));
+        setUser(thisUser.length == 0 ? null : thisUser[0]);
       } else {
         console.log('Fetching queried user');
+        await DataStore.query(User, id).then(setUser);
       }
-      subscription = DataStore.observeQuery(
-        User, u => u.authUserId.eq( id ))
-          .subscribe(({ items }) => items.length == 1 
-            ? setUser(items[0]) 
-            : setUser(null)
-      );
       setIsLoading(false);
-      return subscription;
     }
-
-    const subscription = fetchUser();
-    return () => subscription.unsubscribe();
+    fetchUser();
   }, [id]);
 
   return { user, isLoading }
